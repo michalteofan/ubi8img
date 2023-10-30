be99af802d668fa9a2c2861d8303cb62c6633e33
Michal <michal@pl.ibm.com>
2023-10-30 12:22:49 +0100

    u

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 echo "To sie wykona przy starcie kontenera"
-echo "Moge cos wykonac lub zainstalowac z zamontowanego wolumeny w /mnt/install"
+echo "Moge cos wykonac lub zainstalowac z zamontowanego wolumenu w /mnt/install"
 echo "I czyms sie zajmuje, aby dzialac"
 tail -f /dev/null
